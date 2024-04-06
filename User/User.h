46f28c411d4c0f1e95d1e1e27289a9c00f6b3b70
Martin Gatev <115610146+MGatev@users.noreply.github.com>
2024-04-06 23:55:29 +0300

    Update User.h

--- User/User.h
+++ User/User.h
@@ -1,1 +1,41 @@
+#pragma once
+#include "MyString.h"
+#include <fstream>
 
+class User {
+	MyString firstName;
+	MyString lastName;
+	MyString password;
+	size_t id;
+	int points;
+
+	bool isLoggedIn = false;
+
+	static size_t idCount;
+
+public:
+
+	User();
+	User(const MyString& firstName, const MyString& lastName, const MyString& password);
+
+	const MyString& getFirstName()const;
+	const MyString& getLastName()const;
+	const MyString& getPassword()const;
+	bool getIsLoggedIn()const;
+	size_t getID()const;
+	int getPoints()const;
+
+	void setFirstName(const MyString& firstName);
+	void setLastName(const MyString& lastName);
+	void setPassword(const MyString& password);
+	void setId(const size_t id);
+	void setPoints(const int points);
+
+	void changeIsLoggedIn();
+	void printUser()const;
+	void readFromBinaryFile(std::ifstream& input);
+	void writeToBinaryFile(std::ofstream& output) const;
+
+	static void readIdCountFromBinary(std::ifstream& input);
+	static void writeIdCountToBinary(std::ofstream& output);
+};
